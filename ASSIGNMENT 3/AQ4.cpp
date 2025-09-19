9ae6d84c43c1203692c732461b5cf5062eb86295
Pujan Patel <pujan.patel15122006@gmail.com>
2025-09-19 10:30:13 +0530

    Update AQ4.cpp

--- ASSIGNMENT 3/AQ4.cpp
+++ ASSIGNMENT 3/AQ4.cpp
@@ -1,50 +1,48 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-
-int temperatures[100];
-int stackIndex[100];
-int answer[100];
-int top = -1;
-
-void push(int idx) {
-    stackIndex[++top] = idx;
+#define n 100
+class Stack{
+	int arr[n];
+	int top=-1;
+public:
+	void push(int num){
+		if(top==n-1){
+			cout<<"Stack Full\n";
+			return;
+		}
+		arr[++top]=num;
+	}
+	int pop(){
+		if(top==-1){
+			cout<<"Stack Empty\n";
+			return -1;
+		}
+		return arr[top--];
+	}
+	int peek(){
+		if(top==-1) return -1;
+		return arr[top];
+	}
+	int isEmpty(){
+		return (top==-1);
+	}
+};
+void dailyTemperatures(int temp[],int size,int ans[]){
+	Stack s;
+	for(int i=0;i<size;i++){
+		while(!s.isEmpty()&&temp[i]>temp[s.peek()]){
+			int prev=s.pop();
+			ans[prev]=i-prev;
+		}
+		s.push(i);
+	}
 }
-
-int pop() {
-    return stackIndex[top--];
-}
-
-int peek() {
-    return stackIndex[top];
-}
-
-bool isEmpty() {
-    return top == -1;
-}
-
-int main() {
-    int n;
-    cout << "Enter number of days: ";
-    cin >> n;
-
-    cout << "Enter temperatures: ";
-    for (int i = 0; i < n; i++) {
-        cin >> temperatures[i];
-        answer[i] = 0; 
-    }
-
-    for (int i = 0; i < n; i++) {
-        while (!isEmpty() && temperatures[i] > temperatures[peek()]) {
-            int prevIndex = pop();
-            answer[prevIndex] = i - prevIndex;
-        }
-        push(i);
-    }
-
-    cout << "Answer: ";
-    for (int i = 0; i < n; i++) {
-        cout << answer[i] << " ";
-    }
-
-    return 0;
+int main(){
+	int temp[]={7,6,3,3,53,5,30};
+	int size=sizeof(temp)/sizeof(temp[0]);
+	int ans[n]={0};
+	dailyTemperatures(temp,size,ans);
+	for(int i=0;i<size;i++) cout<<ans[i]<<" ";
+	cout<<endl;
+	return 0;
 }
