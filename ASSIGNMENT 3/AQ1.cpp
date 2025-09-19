773d99769e41be40a953f7ad5910ab6c44ea55d2
Pujan Patel <pujan.patel15122006@gmail.com>
2025-09-19 10:28:36 +0530

    Update AQ1.cpp

--- ASSIGNMENT 3/AQ1.cpp
+++ ASSIGNMENT 3/AQ1.cpp
@@ -1,51 +1,30 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-
-int n;
-int arr[100];
-int stackArr[100];
-int top = -1;
-
-void push(int val) {
-    stackArr[++top] = val;
-}
-void pop() {
-    top--;
-}
-int peek() {
-    return stackArr[top];
-}
-bool isEmpty() {
-    return top == -1;
-}
-
-int main() {
-    cout << "enter size of array:";
-    cin >> n;
-
-    cout << "enter array elements:";
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
-    }
-
-    int need = 1, i = 0;
-
-    while (i < n || !isEmpty()) {
-        if (!isEmpty() && peek() == need) {
-            pop();
-            need++;
-        } 
-        else if (i < n) {
-            push(arr[i]);
-            i++;
-        } 
-        else {
-            break;
+void nse(){
+    int n,m=0;
+    cout<<"Enter size of array: ";
+    cin>>n;
+    int arr[n],ans[n];
+    cout<<"Enter array\n";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+        if(i==0){
+            cout<<-1<<" ";
+        }
+        else{
+            for(int j=i-1;j>=0;j--){
+                m=0;
+                if(arr[j]<arr[i]){
+                    cout<<arr[j]<<" ";
+                    m=1;
+                    break;
+                }
+            }
+            if(m==0) cout<<-1<<" ";
         }
     }
-
-    if (need == n + 1) cout << "YES";
-    else cout << "NO";
-
+}
+int main(){
+    nse();
     return 0;
 }
